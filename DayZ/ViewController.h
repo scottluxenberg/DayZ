0f47724accef8f61191a01fdc1e74b0433cf15e0
Scott Luxenberg <Luxenberg_Scott@bah.com>
2014-03-23 21:33:25 -0400

    Initial commit

---
+++ DayZ/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DayZ
+//
+//  Created by Scott Luxenberg on 2/23/14.
+//  Copyright (c) 2014 Scott Luxenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DayZ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DayZ
+//
+//  Created by Scott Luxenberg on 2/23/14.
+//  Copyright (c) 2014 Scott Luxenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
